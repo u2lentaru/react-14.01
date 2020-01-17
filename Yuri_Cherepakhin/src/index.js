e224ac94ad193974914d94f51900d810650122ae
u2 <u2@lenta.ru>
2020-01-17 15:54:56 +0500

    index.js modified

--- Yuri_Cherepakhin/src/index.js
+++ Yuri_Cherepakhin/src/index.js
@@ -2,8 +2,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Link(props) {
-    return React.createElement("a", {"href": "https://"+props.href}, props.title);
+const messages = [
+    {name: "Ivan", content: "Hello"},
+    {name: "Ivan2", content: "Hello2"},
+    {name: "Ivan3", content: "Hello3"}
+]
+
+const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>;
+
+
+function MessageList({messages}) {
+    return messages.map((message, index) => <Message {...message} key={index}/>)
 }
 
-ReactDom.render(React.createElement(Link, {title: "Это ссылка", href: "google.ru"}),document.getElementById("root"));
+//const MessageList = ({messages}) => {
+//    return messages.map((message) => <Message name={message.name} content={message.content}/>)
+//}
+
+ReactDom.render(<MessageList messages={messages}/>, document.getElementById("root"));
+
+//const Link = (title, link) => <a href={"https://" + link}>{title}</a>
+    //return React.createElement("a", {"href": "https://"+props.href}, props.title);
+
+//ReactDom.render(<Link title="Это ссылка" link="google.ru"/>,document.getElementById("root"));
+//ReactDom.render(React.createElement(Link, {title: "Это ссылка", link: "google.ru"}),document.getElementById("root"));
