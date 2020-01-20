987654c4ab775043c9826d9a83f933485b1442e4
u2 <u2@lenta.ru>
2020-01-20 16:09:20 +0500

    MessageFiel.jsx modified

--- Yuri_Cherepakhin/src/components/MessageFiel.jsx
+++ Yuri_Cherepakhin/src/components/MessageFiel.jsx
@@ -1,1 +1,29 @@
 import React from 'react';
+import Message from './Message';
+import { render } from 'react-dom';
+
+export default class MessageField extends React.Component {
+    state = {
+        messages: [
+            {name:"Ivan", content:"Hello!"},
+            {name:"Alex", content:"Hi!"},
+            {name:"Ivan", content:"Ok."}
+        ]
+    };
+
+
+    handleClick = () => {
+    this.setState({messages: [ ...this.state.messages, 'Good!'] });
+};
+
+render() {
+    const MessageElemets = this.state.messages.map((text,index) => 
+    (<Message key = {index} text = {text} /> ));
+
+    return <div>
+        { MessageElemets } 
+        <button onClick={this.handleClick}>Send Message</button>
+    </div>
+
+    }
+}
