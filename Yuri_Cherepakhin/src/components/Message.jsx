b0c4201e267f2b566bdb31eabaaecc8f3453b848
u2 <u2@lenta.ru>
2020-01-20 15:11:26 +0500

    npm i @babel/plugin-proposal-class-properties

--- Yuri_Cherepakhin/src/components/Message.jsx
+++ Yuri_Cherepakhin/src/components/Message.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class Message extends React.Component {
     render() {
-        return <div><strong></strong></div>;
+        const {name,content} = this.props;
+        return <div><strong>{name}</strong>{content}</div>;
     }
 }
