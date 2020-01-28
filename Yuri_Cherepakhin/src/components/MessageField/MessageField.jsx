7d6148762ffb23e0f5c3967f8ce24001bb4335d1
u2 <u2@lenta.ru>
2020-01-28 12:58:30 +0500

    MessageField.jsx done

--- Yuri_Cherepakhin/src/components/MessageField/MessageField.jsx
+++ Yuri_Cherepakhin/src/components/MessageField/MessageField.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Message from '../Message/Message';
 import './MessageField.css';
+import PropTypes from 'prop-types';
 
 export default class MessageField extends React.Component {
     state = {
@@ -9,7 +10,7 @@ export default class MessageField extends React.Component {
             {name:"Alex", content:"Hi!"},
             {name:"Ivan", content:"Ok."}
         ],
-        input: '',
+        input: ''
     };
 
 
@@ -18,7 +19,8 @@ export default class MessageField extends React.Component {
     //};
 
     handleClick = (message) => {
-        this.sendMessage(message)
+        this.sendMessage(message);
+        //console.log(message);
     };
 
     handleChange = (event) => {
@@ -28,13 +30,12 @@ export default class MessageField extends React.Component {
     handleKeyUp = (event, message) => {
         if (event.keyCode === 13) {
             this.sendMessage(message)
-        }
+        };
     };
 
     sendMessage = (message) => {
         this.setState({ 
-            messages: [ ...this.state.messages, {name: 'me', content: message}],
-            input: '',
+            messages: [ ...this.state.messages, {name: 'me', content: message}], input: ''
         });
     };
 
@@ -52,9 +53,9 @@ render() {
 
     return <div id='main' className='message-field'>
         {MessageElements} 
-        <input onChange = {this.handleChange} value={ this.state.input } 
-        onKeyUp = { (event) => handleKeyUp(event, this.state.input) } />
-        <button onClick={this.handleClick}>Send Message</button>
+        <input value={ this.state.input } onChange = {this.handleChange} 
+        onKeyUp = { (event) => this.handleKeyUp(event, this.state.input) } />
+        <button onClick={ () => this.handleClick(this.state.input)}>Send Message</button>
     </div>
     }
 }
