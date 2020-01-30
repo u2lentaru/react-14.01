e4e1255e2125cc1d3f3c5c05d8210579b8ce1991
u2 <u2@lenta.ru>
2020-01-30 21:40:18 +0500

    MessageField.jsx modified

--- Yuri_Cherepakhin/src/components/MessageField/MessageField.jsx
+++ Yuri_Cherepakhin/src/components/MessageField/MessageField.jsx
@@ -3,14 +3,18 @@ import Message from '../Message/Message';
 import './MessageField.css';
 import { TextField, FloatingActionButton } from 'material-ui';
 import SendIcon from 'material-ui/svg-icons/content/send';
-
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export default class MessageField extends React.Component {
     constructor(props) {
         super(props);
         this.textinput = React.createRef();
     }
+
+    static propTypes = {
+        chatId: PropTypes.number.isRequired,
+    }
+
     state = {
         messages: [
             {name:"Ivan", content:"Hello!"},
@@ -24,14 +28,9 @@ export default class MessageField extends React.Component {
         this.textinput.current.focus();
     }
 
-    //handleClick = () => {
-    //    this.setState(({messages}) => ({messages: [...messages, {name: 'Autor', content: 'Good!'}] }));
-    //};
-
     handleClick = (message) => {
         this.sendMessage(message);
-        //console.log(message);
-    };
+     };
 
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
@@ -49,7 +48,6 @@ export default class MessageField extends React.Component {
         });
     };
 
-    //componentDidUpdate() {
     componentDidUpdate(prevProps, prevState) {        
         if (prevState.messages.length < this.state.messages.length &&
             this.state.messages[this.state.messages.length-1].name !== "Robot") {
