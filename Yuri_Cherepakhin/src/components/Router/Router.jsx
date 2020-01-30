332a08eeddaa6846a2809eed8e38ee514b7b5d8e
u2 <u2@lenta.ru>
2020-01-30 17:20:25 +0500

    Router.jsx, index.jsx modified

--- Yuri_Cherepakhin/src/components/Router/Router.jsx
+++ Yuri_Cherepakhin/src/components/Router/Router.jsx
@@ -3,6 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 
 export default class Router extends React.Component {
-    
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/' component={ Layout } />
+                <Route exact path='/chat/1/' render={ () => 
+                    <Layout chatId={ 1 } /> } />
+                <Route exact path='/chat/2/' render={ () => 
+                    <Layout chatId={ 2 } /> } />
+                <Route exact path='/chat/3/' render={ () => 
+                    <Layout chatId={ 3 } /> } />
+            </Switch>
+        )
+    }
 }
-
