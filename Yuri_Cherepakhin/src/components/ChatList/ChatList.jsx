9351afa945c19c2e3bdf2afe7289b7963894bd6b
u2 <u2@lenta.ru>
2020-01-30 17:27:26 +0500

    Chatlist.jsx modified

--- Yuri_Cherepakhin/src/components/ChatList/ChatList.jsx
+++ Yuri_Cherepakhin/src/components/ChatList/ChatList.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import './Chatlist.css';
 import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import { Link } from 'react-router-dom';
 
 export default class ChatList extends React.Component {
     
     render() {
         return <div className='chatList'>
             <List>
-                <ListItem
-                    primaryText="1st chat"/>
+                <Link to="/chat/1/">
+                    <ListItem primaryText="1st chat"/>
+                </Link>
                 <Divider />
-                <ListItem
-                    primaryText="2nd chat"/>
+                <Link to="/chat/2/">
+                    <ListItem primaryText="2nd chat"/>
+                </Link>
                 <Divider />
-                <ListItem
-                    primaryText="3rd chat"/>
+                <Link to="/chat/3/">
+                    <ListItem primaryText="3rd chat"/>
+                </Link>
                 <Divider />
             </List>
         </div>
