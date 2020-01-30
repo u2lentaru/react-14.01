cdd8d6c666cf0ef3017046c8239cb7855a654d1c
u2 <u2@lenta.ru>
2020-01-30 17:39:16 +0500

    Layout.jsx Header.jsx modified

--- Yuri_Cherepakhin/src/components/Header/Header.jsx
+++ Yuri_Cherepakhin/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Header.css';
+import PropTypes from 'prop-types';
 
 export default class Header extends React.Component {
 
+    static propTypes = {
+        chatId: PropTypes.number,
+    };
+
+    static defaultProps = {
+        chatId: 1,
+    };
+
     render() {
-        return <div className='Header'>It's my Chat!</div>
+        return <div className='Header'>Chat { this.props.chatId } </div>
     }
 
 }
--- Yuri_Cherepakhin/src/components/Layout/Layout.jsx
+++ Yuri_Cherepakhin/src/components/Layout/Layout.jsx
@@ -4,11 +4,21 @@ import Header from '../Header/Header';
 import ChatList from '../ChatList/ChatList';
 import MessageField from '../MessageField/MessageField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import PropTypes from 'prop-types';
 
 export default class Layout extends React.Component {
+
+    static propTypes = {
+        chatId: PropTypes.number,
+    };
+
+    static defaultProps = {
+        chatId: 1,
+    };
+
     render() {
         return <>
-        <Header/>
+        <Header chatId={ this.props.chatId } />
         <div className='wrapper'>
             <MuiThemeProvider>
                 <ChatList/>
