2c92e5db6847a29df18caa3ccdbf1603e9146b67
u2 <u2@lenta.ru>
2020-01-30 21:31:29 +0500

    obj.match.params.chatId

--- Yuri_Cherepakhin/src/components/Router/Router.jsx
+++ Yuri_Cherepakhin/src/components/Router/Router.jsx
@@ -6,13 +6,9 @@ export default class Router extends React.Component {
     render() {
         return (
             <Switch>
-                <Route exact path='/' component={ Layout } />
-                <Route exact path='/chat/1/' render={ () => 
-                    <Layout chatId={ 1 } /> } />
-                <Route exact path='/chat/2/' render={ () => 
-                    <Layout chatId={ 2 } /> } />
-                <Route exact path='/chat/3/' render={ () => 
-                    <Layout chatId={ 3 } /> } />
+                <Route exact={ true } path='/' component={ Layout } />
+                <Route exact path='/chat/:chatId/' render={ obj => 
+                    <Layout chatId={ Number(obj.match.params.chatId) } /> } />
             </Switch>
         )
     }
