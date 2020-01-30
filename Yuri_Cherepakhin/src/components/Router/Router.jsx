a7543e72d46117832d7286ee29564e93820fd48c
u2 <u2@lenta.ru>
2020-01-30 15:20:09 +0500

    Router.jsx modified

---
+++ Yuri_Cherepakhin/src/components/Router/Router.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+
+export default class Router extends React.Component {
+    
+}
+
