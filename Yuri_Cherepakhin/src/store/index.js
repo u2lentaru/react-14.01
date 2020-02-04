5986a56d51d600ffc056577f6f19dd2685eb7aa0
u2 <u2@lenta.ru>
2020-02-04 17:41:03 +0500

    profileReducer.js added

--- Yuri_Cherepakhin/src/store/index.js
+++ Yuri_Cherepakhin/src/store/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import chatReducer from './chatReducer';
+import messageReducer from './messageReducer';
 
 export default combineReducers({
     chatReducer,
+    messageReducer,
+    profileReducer,
 });
