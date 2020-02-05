fbacfae457ee08bdae2bc156fef0b61de4d19f4c
u2 <u2@lenta.ru>
2020-02-05 17:57:42 +0500

    Profile redux done

--- Yuri_Cherepakhin/src/components/Profile/Profile.jsx
+++ Yuri_Cherepakhin/src/components/Profile/Profile.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Profile.css';
+import { bindActionCreators } from 'redux';
+import connect from 'react-redux/es/connect/connect';
 
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
     render() {
         return (
             <div className='Profile'>
-            <h1>Profile</h1>
+            <h1>Profile: { this.props.profile.name }, email: { this.props.profile.email } </h1>
             <Link to='/'>Home</Link>
             </div>
         )
     }
 
 }
+
+
+const mapStateToProps = ({profileReducer}) => ({
+    profile: profileReducer
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
