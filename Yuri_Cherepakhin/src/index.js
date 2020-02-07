90660b05fb9a1d98805a905e97abb497bb7895c5
u2 <u2@lenta.ru>
2020-02-07 15:28:16 +0500

    Task 5 done

--- Yuri_Cherepakhin/src/index.js
+++ Yuri_Cherepakhin/src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from "react-dom";
 import Router from "./components/Router/Router";
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import initStore, { history } from './store/store';
 
+
+const { store, persistor } = initStore();
+
 ReactDOM.render(
-    <Provider store={ initStore() }>
-        <ConnectedRouter history={history}>
-            <MuiThemeProvider>
-                <Router/>
-            </MuiThemeProvider>
-        </ConnectedRouter>
+    <Provider store={ store }>
+        <PersistGate loading={ null } persistor={ persistor } >
+            <ConnectedRouter history={history}>
+                <MuiThemeProvider>
+                    <Router/>
+                </MuiThemeProvider>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>,
  document.getElementById('root'));
--- Yuri_Cherepakhin/src/store/store.js
+++ Yuri_Cherepakhin/src/store/store.js
@@ -1,16 +1,26 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import initReducers from './';
 import middlewares from '../middlewares';
+
+const persistConfig = {
+    key: 'geekmessenger',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whiteList: ['messageReducer', 'chatReducer', 'profileReducer'],
+};
 
 export const history = createBrowserHistory()
 
 function initStore() {
     const initialStore = {};
 
-    return createStore(
-        initReducers(history),
+    const store = createStore(
+        persistReducer(persistConfig, initReducers(history)),
         initialStore,
         compose(
             applyMiddleware(routerMiddleware(history), ...middlewares),
@@ -18,6 +28,10 @@ function initStore() {
         //only for Chrome, not for Opera!
         ),
     );
+
+    const persistor = persistStore(store);
+    
+    return { store, persistor };
 }
 
 export default initStore;
