b3ab5cb810c258a144c1ced81ffa04120a4f3260
u2 <u2@lenta.ru>
2020-02-08 16:44:49 +0500

    Robot answer done

--- Yuri_Cherepakhin/src/middlewares/messageMiddleware.js
+++ Yuri_Cherepakhin/src/middlewares/messageMiddleware.js
@@ -1,10 +1,13 @@
 import { SEND_MESSAGE, sendMessage } from '../store/messageActions';
 
+let timerId = null;
+
 export default store => next => (action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             if (action.sender === 'me') {
-                setTimeout(() => store.dispatch(sendMessage(Object.keys(store.getState().messageReducer.
+                clearTimeout(timerId);
+                timerId = setTimeout(() => store.dispatch(sendMessage(Object.keys(store.getState().messageReducer.
                 messages).length + 1, 'DnD!', 'Robot', action.chatId)), 1000) 
             }
     }
