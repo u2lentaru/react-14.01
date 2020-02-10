4e2c3b3fd29260d8ee42f0e9535de4360181989f
u2 <u2@lenta.ru>
2020-02-10 16:16:33 +0500

    FIRE fixed

--- Yuri_Cherepakhin/src/middlewares/messageMiddleware.js
+++ Yuri_Cherepakhin/src/middlewares/messageMiddleware.js
@@ -15,9 +15,10 @@ export default store => next => (action) => {
                 //next(action);
                 //console.log(action.payload.location.pathname.split('/')[2]);
                 console.log(store.getState().router.location.pathname, ' ', action.chatId);
-                if (store.getState().router.location.pathname === '/chats/' + action.chatId) {
+                if (store.getState().router.location.pathname === '/chat/' + action.chatId) {
                 //if (action.payload.location.pathname === '/chats/' + action.chatId) {
-                    store.dispatch(fire(chatId));
+                    console.log('call fire!');
+                    store.dispatch(fire(action.chatId));
                 }
             }
         }
