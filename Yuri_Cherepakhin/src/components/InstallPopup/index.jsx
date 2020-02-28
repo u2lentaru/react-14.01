2e0cbf66c6b236256ceb46e1688b37c115e443a9
u2 <u2@lenta.ru>
2020-02-28 22:17:41 +0500

    style.css addedd

--- Yuri_Cherepakhin/src/components/InstallPopup/index.jsx
+++ Yuri_Cherepakhin/src/components/InstallPopup/index.jsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import Close from 'material-ui/svg-icons/navigation/close' ;
+import Close from 'material-ui/svg-icons/navigation/close';
 import './styles.css'​;
 
-class InstallPopup extends React.Component {
-​   state = {
-​   isShown​ : false​,
+class InstallPopup extends React.useContext {
+    state = {
+        isShown: false,
     };
 
     componentDidMount() {
-​       // Определяем, является ли устройство iPhone-ом
-​       const isIos = () => {
-​           const userAgent = window.navigator​.​userAgent.​toLowerCase()​;
-​           return ​ /iphone/​.​test​( userAgent )​;
-​       }​;
-​       // Определяем, запущено ли приложение отдельно
-​       const isInStandaloneMode = () => ('standalone' in window.​navigator) &&
-        (window.navigator​.​standalone​)​;
-​       // Решаем, показать или не показать уведомление об установке:
-​       if (isIos​() && ! isInStandaloneMode()) {
-            this.​handleShow​()​;
-​       }
+        const isIos = () => {
+            const userAgent = window.navigator.userAgent.toLowerCase();
+            return /iphone/.test(userAgent);
+        };
+        const isInStandAloneMode = () => ('standalone' in window.navigator) &&
+        (window.navigator.standalone);
+        if (isIos() && !isInStandAloneMode()) {
+            this.handleShow();
+        }
     }
 
-    handleShow = () => { 
-​       this.​setState({ isShown​: true })​;
-​    };
-
-​   handleHide = () => {
-    this.setState({ isShown​: false })​;
-​   };
+    handleShow = () => {
+        this.setState({ isShown: true });
+    };
+    
+    handleHide = () => {
+        this.setState({ isShown: false });
+    };
 
     render() {
         return (
-        <div style ​= ​{{ display​: this.​state​.​isShown ? 'block' : 'none' } }
-        className​ = "speech-bubble-container" >
-        <div className​ = "speech-bubble"​>
-        <Close className​ = "close-install-message-icon" onClick =​ {this​.handleHide}/>
-        <div style​ =​ {{ paddingRight​ : '15px' } }​ >​ Установи приложение
-на свой iPhone: нажми «Поделиться», а затем на экран «Домой»​ </div>
-</div>
-</div>
+            <div style = {{ display: this.state.isShown ? 'block' : 'none' }}
+            className = "speech-bubble-container">
+                <div className = "speech-bubble">
+                    <Close className = "close-install-message-icon" onClick = {this.handleHide}/>
+                    <div style = {{ paddingRight: '15px' }}>
+                        Установи приложение  на свой iPhone: нажми «Поделиться», 
+                        а затем на экран «Домой»​ >
+                    </div>
+                </div>
+            </div>
         );
-​   }
+    }
 }
 
-export default InstallPopup​;
+export default InstallPopup;
