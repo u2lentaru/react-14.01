06e5427a51a6b89c0e07d2342b6f58338cea557b
u2 <u2@lenta.ru>
2020-02-29 22:12:10 +0500

    service-worker.js modified

--- Yuri_Cherepakhin/service-worker.js
+++ Yuri_Cherepakhin/service-worker.js
@@ -59,3 +59,20 @@ self.addEventListener('fetch', function(event) {
       );
     }
 });
+
+self.addEventListener('push', function(event) {
+  
+  console.info('Event: Push');
+
+  var title = 'Тут новый пуш прилетел';
+
+  var body = {
+    'body': 'Нажми сюда, чтобы открыть',
+    'tag': 'pwa',
+    'icon': './manifest/logo-pwa-48.png'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, body)
+  );
+});
